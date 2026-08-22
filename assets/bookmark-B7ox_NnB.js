b333530364691551ed954daa0a8beb35d30d4f2f
yulidar maulana ivan saputra <31232250+yulidarmaulana@users.noreply.github.com>
2026-08-22 13:18:24 +0700

    Updates

---
+++ assets/bookmark-B7ox_NnB.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-C90wwros.js";var t=e(`bookmark`,[[`path`,{d:`M17 3a2 2 0 0 1 2 2v15a1 1 0 0 1-1.496.868l-4.512-2.578a2 2 0 0 0-1.984 0l-4.512 2.578A1 1 0 0 1 5 20V5a2 2 0 0 1 2-2z`,key:`oz39mx`}]]);export{t};
